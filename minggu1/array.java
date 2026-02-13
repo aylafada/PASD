20702a04307ef630cab692d145f2d8fbd4216dc8
aylafada <aylafada7.gmail.com>
2026-02-13 16:00:21 +0700

    percobaaan2

---
+++ minggu1/array.java
@@ -0,0 +1,43 @@
+
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int nilaiPancasila, nilaiKTI, nilaiCTPS, nilaiMatdas, nilaiBing, nilaiDaspro, nilaiK3;
+        int jumlahMK;
+
+        System.out.println("Masukkan jumlah mata kuliah: ");
+        jumlahMK = sc.nextInt();
+        sc.nextLine();
+
+        String[] namaMK = new String[jumlahMK];
+        int [] sks = new int[jumlahMK];
+        String[] nilaiHuruf = new String[jumlahMK];
+        
+
+
+
+        // System.out.println("================================");
+        // System.out.println("Program Menghitung IP Semester");
+        // System.out.println("================================");
+        // System.out.println("Masukkan nilai Angka untuk MK Pancasila: ");
+        // nilaiPancasila = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK Konsep Teknologi Informasi: ");
+        // nilaiKTI = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK Critical Thinking dan Problem Solving: ");
+        // nilaiCTPS = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK Matematika Dasar : ");
+        // nilaiMatdas = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK Bahasa Inggris: ");
+        // nilaiBing = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK Dasar Pemrograman: ");
+        // nilaiDaspro = sc.nextInt();
+        // System.out.println("Masukkan nilai Angka untuk MK keselamatan dan Kesehatan Kerja: ");
+        // nilaiK3 = sc.nextInt();
+
+
+
+    }
+}
